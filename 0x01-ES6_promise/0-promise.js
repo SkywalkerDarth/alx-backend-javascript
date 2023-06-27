2661e2f90cf78040572b9f8b60a227affe1b74bc
SkywalkerDarth <danteuche@gmail.com>
2023-06-27 14:08:01 +0100

    redo

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,8 +1,3 @@
 export default function getResponseFromAPI() {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      const responseData = { message: "API Response" };
-      resolve(responseData);
-    }, 3000);
-  });
+  return new Promise(() => {});
 }
