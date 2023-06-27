80b5a9c2cb2fad4d996e79e63d8c0432b8071065
SkywalkerDarth <danteuche@gmail.com>
2023-06-27 20:41:51 +0100

    more task

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,11 +1,9 @@
 export default function handleResponseFromAPI(promise) {
-  return Promise
-  .then(() => ({
-    status: 200,
-    body: 'Success',
-      }))
-    .catch(() => Error())
-    .finally(() => console.log(
-'Got a response from the API'));
+  return promise
+    .then(() => ({
+      status: 200,
+      body: 'Success',
+    }))
+    .catch(() => new Error())
+    .finally(() => console.log('Got a response from the API'));
 }
-export default handleResponseFromAPI;
