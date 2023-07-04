8598cfd5cd63dbd727c97e134a400075c6b085f9
SkywalkerDarth <danteuche@gmail.com>
2023-07-04 14:36:05 +0100

    fix

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,5 +1,5 @@
 export default function updateUniqueItems(map) {
-  if (!(Map instanceof Map)) {
+  if (!(map instanceof Map)) {
     throw new Error('Cannot process');
   }
   map.forEach((value, key) => {
